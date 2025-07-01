0340f4e1d14e46438747f20c749c861d4ecfb1a9
Banpreet-Aulakh <banpreetaulakh@gmail.com>
2025-06-30 18:36:26 -0700

    Create Initial data model

--- schema.sql
+++ schema.sql
@@ -1,1 +1,38 @@
--- Model -- 
+-- Book Table --
+CREATE TABLE Book (
+    ISBN VARCHAR(20) PRIMARY KEY,
+    Pages INTEGER,
+    Subject VARCHAR(100),
+    CoverURL TEXT,
+    Title TEXT,
+    Author TEXT
+);
+
+-- User Table --
+CREATE TABLE AppUser (
+    ID SERIAL PRIMARY KEY
+);
+
+-- Skill Table --
+CREATE TABLE Skill (
+    SkillName VARCHAR(100) PRIMARY KEY
+);
+
+-- UserBook Table --
+CREATE TABLE UserBook (
+    UserID INTEGER REFERENCES AppUser(ID),
+    ISBN VARCHAR(20) REFERENCES Book(ISBN),
+    SkillName VARCHAR(100) REFERENCES Skill(SkillName),
+    SkillLevel INTEGER,
+    PRIMARY KEY (UserID, ISBN)
+);
+
+-- UserSkill Table --
+CREATE TABLE UserSkill (
+    SkillName VARCHAR(100) REFERENCES Skill(SkillName),
+    UserID INTEGER REFERENCES AppUser(ID),
+    ExpGained INTEGER DEFAULT 0,
+    ExpToNext INTEGER DEFAULT 0,
+    SkillLevel INTEGER,
+    PRIMARY KEY (SkillName, UserID)
+);
