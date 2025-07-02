23c13ce7499678ddeedc67769d75fd933fc20fb2
Banpreet-Aulakh <banpreetaulakh@gmail.com>
2025-07-01 19:35:23 -0700

    Complete home page

--- index.js
+++ index.js
@@ -24,18 +24,24 @@ app.use(express.urlencoded({ extended: true }));
 let userId = 1; // placeholder for more users
 
 app.get("/", async (req, res) => {
-  const result = await db.query(
-    `SELECT b.*, ub.PagesRead, us.ExpGained
+  try {
+    const result = await db.query(
+      `SELECT b.*, ub.PagesRead, us.ExpGained, us.ExpToNext
      FROM UserBook ub
      JOIN Book b ON ub.ISBN = b.ISBN
      JOIN UserSkill us ON us.UserID = ub.UserID AND us.SkillName = ub.SkillName
      WHERE ub.UserID = $1
      ORDER BY ub.LastUpdated DESC
      LIMIT 3`,
-    [userId]
-  );
-  console.log(result.rows);
-  res.render("index.ejs");
+      [userId]
+    );
+
+    console.log(result.rows);
+    res.render("index.ejs", { data: result.rows });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.get("/library", async (req, res) => {
