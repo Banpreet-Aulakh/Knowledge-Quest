7b0c0efcc3220cbf2de2ff95452c7c79f3b62743
Banpreet-Aulakh <banpreetaulakh@gmail.com>
2025-07-14 21:54:39 -0700

    Change db to accept both the local and remote dbs

--- db.js
+++ db.js
@@ -10,9 +10,10 @@ dotenv.config();
 //   database: process.env.DB_NAME,
 // });
 
-const db = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL || 
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
 
 db.connect();
